be847c999539db16ef339be5715f0e7668d37ec6
zmpeg <zandstra.matt@gmail.com>
2016-12-06 11:01:30 -0700

    renamed function

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 zip -r function.zip index.js rasterize.js node_modules phantomjs-linux
 
 aws lambda update-function-code \
---function-name abmi-print \
+--function-name rasterize \
 --zip-file fileb://function.zip
 
 rm function.zip
