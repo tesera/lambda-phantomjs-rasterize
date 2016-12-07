e3c2d3a82adb5b4b378032b78ec59d23c89d7ad3
zmpeg <zandstra.matt@gmail.com>
2016-12-07 13:10:58 -0700

    fix test script

--- test.sh
+++ test.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 
 curl \
-    --request POST \
-    -H "Accept: image/png" \
-    -H "Content-type: application/x-www-form-urlencoded" \
-    --data "url=http://dev.abmi-map-portal.s3-website-us-east-1.amazonaws.com/land-cover/human-footprint/energy" \
-    https://198sy86655.execute-api.us-east-1.amazonaws.com/prod > img-form.png
-
-
-curl \
-    --request POST \
-    -H "Accept: image/png" \
-    -H "Content-type: application/json" \
-    --data '{"url":"http://dev.abmi-map-portal.s3-website-us-east-1.amazonaws.com/land-cover/human-footprint/energy"}' \
-    https://198sy86655.execute-api.us-east-1.amazonaws.com/prod > img-json.png
+   --request POST \
+   -H "Accept: image/png" \
+   -H "Content-type: application/json" \
+   --data '{"url":"http://www.tesera.com/"}' \
+   https://198sy86655.execute-api.us-east-1.amazonaws.com/dev > img-json.png
