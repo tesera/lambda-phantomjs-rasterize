da2718796fbce6bec452edeab921532fb2c792d1
Yves Richard <yves.richard@tesera.com>
2017-02-09 10:40:52 -0700

    Add resize option

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ zip -r function.zip index.js rasterize.js node_modules phantomjs-linux
 
 aws lambda update-function-code \
 --function-name rasterize \
---zip-file fileb://function.zip
+--zip-file fileb://function.zip \
+--region us-west-2
 
 rm function.zip
--- index.js
+++ index.js
@@ -2,24 +2,38 @@
 const fs = require('fs');
 const path = require('path');
 const execFile = require('child_process').execFile;
+const sharp = require('sharp');
 
 exports.handler = (event, context, callback) => {
     console.log(event);
     const phantomJSPath = path.resolve("phantomjs-linux");
     const rasterizePath = path.resolve('rasterize.js');
-    const tmpImagePath = '/tmp/export.png';
-    
+    const exportedImagePath = '/tmp/exported.png';
+    const resizedImagePath = '/tmp/resized.png';
+
     if(!event.params.querystring.url) return callback("Could not find URL to rasterize.");
 
     const url = decodeURIComponent(event.params.querystring.url);
-    const size = event.params.querystring.size || '1440px*900px';
+    const screenSize = event.params.querystring.size ? decodeURIComponent(event.params.querystring.screenSize) : '1440px*900px';
+    const outWidth = event.params.querystring.outWidth ? event.params.querystring.outWidth.replace('px', '') : '';
 
-    execFile(phantomJSPath, [rasterizePath, url, tmpImagePath, size], (err, stdout, stderr) => {
+    execFile(phantomJSPath, [rasterizePath, url, exportedImagePath, size], (err, stdout, stderr) => {
+        let exported;
         if (err) {
             console.error(`exec error: ${err}`);
             return callback(err, {statusCode: 500, body: 'ERROR'});
         } else {
-            callback(null, fs.readFileSync(tmpImagePath).toString('base64'));
+            exported = fs.readFileSync(exportedImagePath);
+            if (outWidth) {
+                sharp(exported)
+                    .resize(outWidth, null)
+                    .toFile(resizedImagePath, (err, info) => {
+                        if (err) callback(err);
+                        else callback(null, fs.readFileSync(resizedImagePath).toString('base64')); 
+                    });
+            } else {
+                callback(null, exported.toString('base64'));
+            }
         }
     });
 };
